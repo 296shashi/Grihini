8cf0f200bf9818cdaa568b8647185fd14371a783
296shashi <296shashi@gmail.com>
2020-07-19 01:15:56 +0530

    Folder structure

---
+++ grihini/src/main/java/com/example/grihini/GrihiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.grihini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrihiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrihiniApplication.class, args);
+	}
+
+}
---
+++ grihini/src/test/java/com/example/grihini/GrihiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grihini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrihiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
